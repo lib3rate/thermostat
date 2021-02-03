8b6d29b7759061eda9b630f847d0d6c9fa13db52
lib3rate <dmitry.petrov0918@gmail.com>
2021-02-02 20:42:55 -0500

    Update Navigation and Unit state, reducers and selectors

--- src/components/Unit/unitSlice.js
+++ src/components/Unit/unitSlice.js
@@ -3,43 +3,53 @@ import { createSlice } from '@reduxjs/toolkit';
 export const unitSlice = createSlice({
   name: 'unit',
   initialState: {
-    selectedUnit: null,
+    currentUnit: null,
     isTurnedOn: false,
     mode: null,
+    currentTemperature: null,
     desiredTemperature: null,
   },
   reducers: {
-    increment: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+    changeUnit: (state, action) => {
+      state.currentUnit = action.payload;
     },
-    decrement: state => {
-      state.value -= 1;
+    switchPower: state => {
+      state.isTurnedOn ? state.isTurnedOn = false : state.isTurnedOn = true;
     },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    changeMode: (state, action) => {
+      state.mode = action.payload;
     },
+    increaseDesiredTemperature: state => {
+      state.desiredTemperature += 1;
+    },
+    decreaseDesiredTemperature: state => {
+      state.desiredTemperature -= 1;
+    }
   },
 });
 
-export const { increment, decrement, incrementByAmount } = unitSlice.actions;
+export const {
+  changeUnit,
+  switchPower,
+  changeMode,
+  increaseDesiredTemperature,
+  decreaseDesiredTemperature
+} = unitSlice.actions;
 
 // The function below is called a thunk and allows us to perform async logic. It
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched
-export const incrementAsync = amount => dispatch => {
-  setTimeout(() => {
-    dispatch(incrementByAmount(amount));
-  }, 1000);
-};
+// export const incrementAsync = amount => dispatch => {
+//   setTimeout(() => {
+//     dispatch(incrementByAmount(amount));
+//   }, 1000);
+// };
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectCount = state => state.counter.value;
+export const selectCurrentUnit = state => state.currentUnit.value;
+export const isTurnedOn = state => state.isTurnedOn.value;
+export const selectMode = state => state.mode.value;
+export const selectCurrentTemperature = state => state.currentTemperature.value;
+export const selectDesiredTemperature = state => state.desiredTemperature.value;
 
 export default unitSlice.reducer;
